19964cfa3016b02dcbac37e76add8b9398d37ce0
JoySquare <phelahab@gmail.com>
2020-05-01 13:32:42 -0400

    modified `save` and `load` method to handle arbitrary inputs (to save/load `alpha` tesnsor)

--- runner/base.py
+++ runner/base.py
@@ -11,11 +11,11 @@ class BaseRunner(object):
         self.epoch = 0
         self.G = model
         self.optim = optim
-        self.lr_schler = lr_schdlr
+        self.lr_schdlr = lr_schdlr
         self.best_metric = 0
         self.load()
 
-    def save(self, epoch, metric, file_name="model"):
+    def save(self, epoch, metric, file_name="model", **kwargs):
         save_path = Path(self.save_path)
         save_path.mkdir(parents=True, exist_ok=True)
 
@@ -24,8 +24,8 @@ class BaseRunner(object):
                     "optimizer": self.optim.state_dict(),
                     "score": metric,
                     "best": self.best_metric,
-                    "lr_schdlr": self.lr_schler.state_dict()
-                    }, f"{save_path}/{file_name}.pth")
+                    "lr_schdlr": self.lr_schdlr.state_dict(),
+                    **kwargs}, f"{save_path}/{file_name}.pth")
 
         if metric >= self.best_metric:
             print(f"{self.best_metric} -------------------> {metric}")
@@ -41,14 +41,23 @@ class BaseRunner(object):
             print(f"Load {save_path} File")
             ckpoint = torch.load(f"{save_path}/{file_name}")
 
-            self.G.load_state_dict(ckpoint['param'])
-            self.optim.load_state_dict(ckpoint['optimizer'])
-            self.epoch = ckpoint['epoch']
-            self.best_metric = ckpoint["score"]
-            self.lr_schler.load_state_dict(ckpoint['lr_schdlr'])
+            for key, value in ckpoint.items():
+                if key == 'param':
+                    self.G.load_state_dict(value)
+                elif key == 'optimizer':
+                    self.optim.load_state_dict(value)
+                elif key == 'lr_schdlr':
+                    self.lr_schdlr.load_state_dict(value)
+                elif key == 'epoch':
+                    self.epoch = value
+                elif key == 'score':
+                    self.best_metric = value
+                else:
+                    self.__dict__[key] = value
+
             print(f"Load Model Type : {file_name}, epoch : {self.epoch}")
         else:
             print("Load Failed, not exists file")
 
     def get_lr(self):
-        return self.lr_schler.optimizer.param_groups[0]['lr']
+        return self.lr_schdlr.optimizer.param_groups[0]['lr']
