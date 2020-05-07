753a4f22076feece6e5754e2693bff7eb9bd6640
JoySquare <phelahab@gmail.com>
2020-05-07 09:05:02 -0400

    removed vgg module

--- models/__init__.py
+++ models/__init__.py
@@ -6,7 +6,6 @@ from torchvision import models
 
 MODEL_DICT = {'lenet': [lenet, 'layer2', 5*5*64],
               'alexnet': [models.alexnet, 'avgpool', 256*6*6],
-              'vgg13': [models.vgg13_bn, 'avgpool', 512*7*7],
               'resnet50': [models.resnet50, 'avgpool', 2048],
               'squeeze1_0': [models.squeezenet1_0, 'features', 512],
               'mnasnet0_5': [models.mnasnet0_5, 'layers', 1280]}
