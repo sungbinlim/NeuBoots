914c9d3f4cb46e0dbcf282615685e6de4c3a71bf
NySunShine <phelahab@gmail.com>
2020-05-18 02:27:50 +0900

    added wideresnet

--- models/__init__.py
+++ models/__init__.py
@@ -1,15 +1,19 @@
 import torch
-from .cnn import lenet
-from .gbsnet import gbs_conv, GbsCls
 
 from torchvision import models
+
+from .cnn import lenet
+from .gbsnet import gbs_conv, GbsCls
+from .wideresnet import wresnet28_2, wresnet28_10
 
 
 MODEL_DICT = {'lenet': [lenet, 'layer2', 6*6*64],
               'alexnet': [models.alexnet, 'avgpool', 256*6*6],
               'resnet50': [models.resnet50, 'avgpool', 2048],
               'squeeze1_0': [models.squeezenet1_0, 'features', 512],
-              'mnasnet0_5': [models.mnasnet0_5, 'layers', 1280]}
+              'mnasnet0_5': [models.mnasnet0_5, 'layers', 1280],
+              'wresnet28_2': [wresnet28_2, 'avgpool', 128],
+              'wresnet28_10': [wresnet28_10, 'avgpool', 640]}
 
 
 def _get_model(model_name, hidden_size, n_a, num_layer,
