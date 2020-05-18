9914ed93f51d2945b6359b3d87d97323d0878b1c
NySunShine <phelahab@gmail.com>
2020-05-18 17:32:53 +0900

    added flag for using extra dataset  of svhn

--- data/data_loader.py
+++ data/data_loader.py
@@ -20,13 +20,14 @@ def _get_split_indices(trainset, p, seed):
 class GbsDataLoader(object):
     def __init__(self, dataset, batch_size, n_a, sub_size, cpus, seed=0):
         self.dataset = self._get_dataset(dataset)
-        self.split_indices = _get_split_indices(self.dataset['trainset'], 0.2, seed)
-        self.p = self.dataset['trainset'][0][0].nelement()
+        self.split_indices = _get_split_indices(self.dataset['train'],
+                                                0.2, seed)
+        self.p = self.dataset['train'][0][0].nelement()
         self.sub_size = sub_size
         self.n_a = n_a
         self.n_train = len(self.split_indices[0])
         self.n_val = len(self.split_indices[1])
-        self.n_test = len(self.dataset['testset'])
+        self.n_test = len(self.dataset['test'])
         self.n_b = self.n_train // n_a
         self.batch_size = batch_size
         self.cpus = cpus
@@ -44,18 +45,20 @@ class GbsDataLoader(object):
     def _train(self):
         self.len = ceil(self.n_train / self.batch_size)
         sampler = SubsetRandomSampler(self.split_indices[0])
-        loader = DataLoader(self.dataset['trainset'], batch_size=self.batch_size,
-                            sampler=sampler, num_workers=self.cpus, pin_memory=True)
+        loader = DataLoader(self.dataset['train'], batch_size=self.batch_size,
+                            sampler=sampler, num_workers=self.cpus,
+                            pin_memory=True)
         return loader
 
     def _val(self):
         sampler = SubsetRandomSampler(self.split_indices[1])
-        loader = DataLoader(self.dataset['trainset'], batch_size=self.batch_size,
-                            sampler=sampler, num_workers=self.cpus, pin_memory=True)
+        loader = DataLoader(self.dataset['train'], batch_size=self.batch_size,
+                            sampler=sampler, num_workers=self.cpus,
+                            pin_memory=True)
         return loader
 
     def _test(self):
-        loader = DataLoader(self.dataset['testset'], batch_size=self.batch_size,
+        loader = DataLoader(self.dataset['test'], batch_size=self.batch_size,
                             num_workers=self.cpus, pin_memory=True)
         return loader
 
@@ -63,39 +66,45 @@ class GbsDataLoader(object):
         _d = {'cifar10': lambda: self._load_cifar10(),
               'cifar100': lambda: self._load_cifar100(),
               'mnist': lambda: self._load_mnist(),
-              'svhn': lambda: self._load_svhn()}
+              'svhn': lambda: self._load_svhn(),
+              'svhn_extra': lambda: self._load_svhn(use_extra=True)}
         try:
             _dataset = _d[dataset]()
             return _dataset
         except:
-            raise ValueError('Dataset should be one of [mnist, cifar10, cifar100, svhn]')
+            raise ValueError(
+                'Dataset should be one of [mnist, cifar10, cifar100, svhn]'
+                )
 
     def _load_mnist(self):
         trainset = MNIST(root='.mnist', train=True, download=True,
                          transform=transforms.ToTensor())
         testset = MNIST(root='.mnist', train=False, download=True,
                         transform=transforms.ToTensor())
-        return {'trainset': trainset, 'testset': testset}
+        return {'train': trainset, 'test': testset}
 
     def _load_cifar10(self):
         trainset = CIFAR10(root='.cifar10', train=True, download=True,
                            transform=transform_train)
         testset = CIFAR10(root='.cifar10', train=False, download=True,
                           transform=transform_test)
-        return {'trainset': trainset, 'testset': testset}
+        return {'train': trainset, 'test': testset}
 
     def _load_cifar100(self):
         trainset = CIFAR100(root='.cifar100', train=True, download=True,
                             transform=transform_train)
         testset = CIFAR100(root='.cifar100', train=False, download=True,
                            transform=transform_test)
-        return {'trainset': trainset, 'testset': testset}
+        return {'train': trainset, 'test': testset}
 
-    def _load_svhn(self):
+    def _load_svhn(self, use_extra=False):
         trainset = SVHN(root='.svhn', split='train', download=True,
                         transform=transform_train)
         extraset = SVHN(root='.svhn', split='extra', download=True,
                         transform=transform_train)
         testset = SVHN(root='.svhn', split='test', download=True,
                        transform=transform_test)
-        return {'trainset': trainset + extraset, 'testset': testset}
+        if use_extra:
+            return {'train': trainset + extraset, 'test': testset}
+        else:
+            return {'train': trainset, 'test': testset}
