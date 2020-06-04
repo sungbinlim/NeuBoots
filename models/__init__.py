f382359b7db2db87197a5f62d2602a534a629c76
Hyungjoo Cho <phelahab@gmail.com>
2020-06-05 04:38:04 +0900

    added mlp option

--- models/__init__.py
+++ models/__init__.py
@@ -8,7 +8,8 @@ from .gbsnet import gbs_conv, GbsCls
 from .wideresnet import wresnet28_2, wresnet28_10, wresnet16_8
 
 
-MODEL_DICT = {'alexnet': [models.alexnet, 'avgpool', 256 * 6 * 6],
+MODEL_DICT = {'mlp': [None, 'none', 28 * 28 * 3],
+              'alexnet': [models.alexnet, 'avgpool', 256 * 6 * 6],
               'resnet34': [ResNet34, 'layer4', 512],
               'resnet50': [models.resnet50, 'avgpool', 2048],
               'densenet100': [densenet100, 'bn1', 342],
@@ -26,4 +27,7 @@ def _get_model(model_name, hidden_size, n_a, num_layer,
         classifier = GbsCls(in_feat, hidden_size, num_layer, n_a, num_classes)
     else:
         classifier = torch.nn.Linear(in_feat, num_classes)
-    return gbs_conv(backbone, return_layer, classifier, is_gbs, dropout_rate)
+    if backbone:
+        return gbs_conv(backbone, return_layer, classifier, is_gbs, dropout_rate)
+    else:
+        return classifier
