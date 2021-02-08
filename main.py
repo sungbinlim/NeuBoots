689e6969f1af762e254bc1e3b7adf947d2801456
Hyungjoo Cho <phelahab@gmail.com>
2021-02-08 18:50:48 +0900

    add seg module

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import argparse
 
 from utils.logger import get_logger
 from utils.arg_parser import Argments
-from loader.data_loader import GeneralDataLoader, NbsDataLoader
+from loader.data_loader import GeneralDataLoaderCls, NbsDataLoaderCls,\
+                               GeneralDataLoaderSeg, NbsDataLoaderSeg
 from runners.cnn_runner import CnnRunner
 from runners.nbs_runner import NbsRunner
+from runners.mcd_runner import McdRunner
 
 
 def main():
     argparser = argparse.ArgumentParser()
     argparser.add_argument("yaml")
     argparser.add_argument("--phase", default="train", type=str)
-    argparser.add_argument("--seed", default=-1, type=int)
+    argparser.add_argument("--index", default=-1, type=int)
     argparser.add_argument("--gpus", default="-1", type=str)
     argparser.add_argument("--local_rank", default=0, type=int)
     cmd_args = argparser.parse_args()
@@ -26,25 +28,41 @@ def main():
 
     model_type = setup['model_type']
     dataset = arg['path/dataset']
-    if model_type == 'nbs':
-        data_loader = NbsDataLoader(dataset, setup['batch_size'],
-                                    setup['n_a'], setup['cpus'], setup['seed'])
+    is_seg = False
+    if 'nbs' in model_type:
+        if 'seg' in model_type:
+            is_seg = True
+            _data_loader = NbsDataLoaderSeg
+        else:
+            _data_loader = NbsDataLoaderCls
+        data_loader = _data_loader(dataset, setup['batch_size'],
+                                   setup['n_a'], setup['cpus'], setup['seed'])
         runner = NbsRunner(data_loader, **arg.module, num_epoch=setup['num_epoch'],
                            logger=logger, model_path=model_path, rank=setup['rank'],
                            epoch_th=setup['epoch_th'], num_mc=setup['num_mc'],
                            adv_training=setup['adv_training'])
     else:
-        data_loader = GeneralDataLoader(dataset, setup['batch_size'],
-                                        setup['cpus'], setup['seed'])
-        runner = CnnRunner(data_loader, **arg.module, num_epoch=setup['num_epoch'],
-                           logger=logger, model_path=model_path, rank=setup['rank'],
-                           adv_training=setup['adv_training'])
+        if 'seg' in model_type:
+            is_seg = True
+            _data_loader = GeneralDataLoaderSeg
+        else:
+            _data_loader = GeneralDataLoaderCls
+        data_loader = _data_loader(dataset, setup['batch_size'],
+                                   setup['cpus'], setup['seed'])
+        if 'mcd' in model_type:
+            runner = McdRunner(data_loader, **arg.module, num_epoch=setup['num_epoch'],
+                               logger=logger, model_path=model_path, rank=setup['rank'],
+                               num_mc=setup['num_mc'], adv_training=setup['adv_training'])
+        else:
+            runner = CnnRunner(data_loader, **arg.module, num_epoch=setup['num_epoch'],
+                            logger=logger, model_path=model_path, rank=setup['rank'],
+                            adv_training=setup['adv_training'])
 
     if setup['phase'] == 'train':
         runner.train()
-        runner.test()
+        runner.test(is_seg)
     elif setup['phase'] == 'test':
-        runner.test()
+        runner.test(is_seg)
 
 
 if __name__ == "__main__":
